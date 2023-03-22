4e97042ec2b9f04e23754a15c18b653dabb9fff3
nour1396 <nourelkashef96@gmail.com>
2023-03-21 17:36:56 -0700

    tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prit all alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(letter);
+	_putchar('\n');
+}
