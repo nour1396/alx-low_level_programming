7ff104cb04032e25b527232abb01488132853ef3
nour1396 <nourelkashef96@gmail.com>
2023-04-05 12:28:03 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,13 +2,13 @@
 
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') {   // base case: end of string
+    if (*s == '\0') {
         putchar('\n');
         return;
     }
     
-    putchar(*s);        // print the current character
-    _puts_recursion(s+1);   // recursively print the rest of the string
+    putchar(*s);
+    _puts_recursion(s+1);
 }
 
 int main()
